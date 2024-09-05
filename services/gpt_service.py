a534da032264e422c80d852a7bd7b1721bb73936
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-09-05 13:30:14 +0300

    update gpt model

--- services/gpt_service.py
+++ services/gpt_service.py
@@ -177,7 +177,7 @@ async def generate_gpt_response(values):
     )
 
     response = client.beta.threads.retrieve(thread.id)
-    print(response)
+    print("RESPONSE", response)
 
     full_response = response.messages[-1]['content']
     sections = full_response.split('\n\n')
