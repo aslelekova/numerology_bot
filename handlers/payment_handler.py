f68af462df2076756f4e514998644ca0851b9c05
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-09-16 13:55:27 +0300

    update

--- handlers/payment_handler.py
+++ handlers/payment_handler.py
@@ -22,7 +22,7 @@ async def handle_full_access(callback_query: CallbackQuery, state: FSMContext):
     await delete_messages(callback_query.bot, callback_query.message.chat.id, [first_message_id, question_prompt_message_id])
 
     # Генерируем ссылки на оплату
-    payment_url_1, payment_id_1 = await create_payment("290.00", callback_query.message.chat.id, "Тариф 1. 290 руб")
+    payment_url_1, payment_id_1 = await create_payment("290.00", callback_query.message.chat.id, "Тариф 1. 1 руб")
     payment_url_2, payment_id_2 = await create_payment("450.00", callback_query.message.chat.id, "Тариф 2. 450 руб")
     payment_url_3, payment_id_3 = await create_payment("650.00", callback_query.message.chat.id, "Тариф 3. 650 руб")
 
