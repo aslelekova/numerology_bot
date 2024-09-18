104feebd4ce7c4ec43c8c90dff1aa4fa120dcfee
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-09-19 01:50:45 +0300

    update

--- handlers/sections_handler.py
+++ handlers/sections_handler.py
@@ -7,10 +7,11 @@ from openai import AsyncOpenAI
 
 import config
 from keyboards.back_to_menu import create_back_button
-from keyboards.sections_fate_matrix import create_sections_keyboard, functions_keyboard
+from keyboards.sections_fate_matrix import create_full_sections_keyboard, create_sections_keyboard, functions_keyboard
 from services.birthday_service import calculate_values
 from services.message_service import delete_messages, send_initial_messages
 from states import QuestionState
+from user_input_handler import get_subscription_details
 
 router = Router()
 
@@ -40,9 +41,35 @@ async def handle_back_button(callback_query: CallbackQuery, state: FSMContext):
 
     await delete_messages(callback_query.bot, callback_query.message.chat.id, [first_message_id, question_prompt_message_id])
 
-    section_message = "Ура, ваша матрица судьбы готова 🔮\n\nВы можете посмотреть расклад по каждому из разделов.\n✅ - доступно бесплатно\n🔐 - требуется полный доступ"
-    question_message = ("Получите <b>ответы на все свои вопросы</b> с ПОЛНЫМ доступом к:\n🔮 Матрице судьбы\n💸 Нумерологии"
-                        " | Личному успеху | Финансам\n💕 Совместимости с партнером\n\nИли <b>задайте любой вопрос</b> нашему "
-                        "персональному ассистенту и получите мгновенный ответ (например: 💕<b>Как улучшить отношения с партнером?</b>)")
+    user_id = callback_query.from_user.id
+    subscription_details = await get_subscription_details(user_id)
+    subscription_active = subscription_details["subscription_active"]
+    readings_left = subscription_details["readings_left"]
+    questions_left = subscription_details["questions_left"]
 
-    await send_initial_messages(callback_query.bot, callback_query.message.chat.id, state, section_message, question_message, create_sections_keyboard(), functions_keyboard())
+    if subscription_active:
+        first_message = await callback_query.message.answer(
+            f"У вас осталось:\n🔮 {readings_left} любых раскладов\n⚡️ {questions_left} ответа на любые вопросы",
+            reply_markup=create_full_sections_keyboard()
+        )
+        await state.update_data(first_message_id=first_message.message_id)
+
+        question_prompt_message = await callback_query.message.answer(
+            f"Сделайте новый расчет:  \n🔮 Матрица судьбы\n💸 Нумерология | Личному успеху | Финансам\n💕 Совместимость с партнером\n\nИли <b>задайте любой вопрос</b> нашему "
+            "персональному ассистенту и получите мгновенный ответ (например: 💕<b>Как улучшить отношения с партнером?</b>)",
+            reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="Новый расчет", callback_data="main_menu")],
+                [InlineKeyboardButton(text="Задать вопрос", callback_data="ask_free_question")],
+                [InlineKeyboardButton(text="Главное меню", callback_data="main_menu")]
+            ]),
+            parse_mode="HTML"
+        )
+        await state.update_data(question_prompt_message_id=question_prompt_message.message_id)
+
+    else:
+        section_message = "Ура, ваша матрица судьбы готова 🔮\n\nВы можете посмотреть расклад по каждому из разделов.\n✅ - доступно бесплатно\n🔐 - требуется полный доступ"
+        question_message = ("Получите <b>ответы на все свои вопросы</b> с ПОЛНЫМ доступом к:\n🔮 Матрице судьбы\n💸 Нумерологии"
+                            " | Личному успеху | Финансам\n💕 Совместимости с партнером\n\nИли <b>задайте любой вопрос</b> нашему "
+                            "персональному ассистенту и получите мгновенный ответ (например: 💕<b>Как улучшить отношения с партнером?</b>)")
+
+        await send_initial_messages(callback_query.bot, callback_query.message.chat.id, state, section_message, question_message, create_sections_keyboard(), functions_keyboard())
