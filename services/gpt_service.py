56a60f88d5afe9b5f15b57f18ba6279a85ac4a42
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-09-25 16:46:02 +0300

    update

--- services/gpt_service.py
+++ services/gpt_service.py
@@ -1,21 +1,21 @@
 import config
-from openai import AsyncOpenAI, AssistantEventHandler
+from openai import AsyncAssistantEventHandler, AsyncOpenAI
 import asyncio
 
 client = AsyncOpenAI(api_key=config.OPENAI_API_KEY)
 
-class EventHandler(AssistantEventHandler):
+class EventHandler(AsyncAssistantEventHandler):
     def __init__(self):
         super().__init__()
         self.response_text = None
 
-    def on_text_created(self, text) -> None:
+    async def on_text_created(self, text) -> None:
         print(f"\nassistant > ", end="", flush=True)
 
-    def on_tool_call_created(self, tool_call):
+    async def on_tool_call_created(self, tool_call):
         print(f"\nassistant > {tool_call.type}\n", flush=True)
 
-    def on_message_done(self, message) -> None:
+    async def on_message_done(self, message) -> None:
         print("Message done called with message:", message)
         if hasattr(message, 'content'):
             message_content = message.content[0].text
