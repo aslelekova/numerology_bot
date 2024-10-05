5f91491cec5efac0ee341518fc1038102898feec
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-10-05 16:40:55 +0300

    update

--- handlers/share_with_friend.py
+++ handlers/share_with_friend.py
@@ -1,16 +1,22 @@
 import uuid
-from fastapi import FastAPI
+
 from aiogram import types
+from aiogram.fsm.context import FSMContext
+from fastapi import FastAPI
 from aiogram import Router
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 from services.db_service import update_questions_left
+from services.message_service import save_message_id
 
 router = Router()
 app = FastAPI()
 
+
 def generate_share_link(user_id: int) -> str:
     unique_token = str(uuid.uuid4())
     return f"https://t.me/MakeMyMatrix_Bot?user_id={user_id}&token={unique_token}"
+
 
 @router.callback_query(lambda callback: callback.data == "share_and_ask")
 async def share_and_ask_handler(callback_query: types.CallbackQuery):
@@ -21,8 +27,27 @@ async def share_and_ask_handler(callback_query: types.CallbackQuery):
         f"Поделитесь этой ссылкой с другом, чтобы они могли задать вам вопрос: {share_link}"
     )
 
+
 @app.get("/share")
-async def share_redirect(user_id: int, token: str):
+async def share_redirect(user_id: int, token: str, callback_query: CallbackQuery, state: FSMContext):
     # Логика проверки токена здесь (если нужно)
+
+    # Обновляем количество доступных вопросов
     await update_questions_left(user_id, 1)
+
+    # Отправляем уведомление пользователю, который поделился ссылкой
+    await notify_user(callback_query, state)
+
     return {"message": "Спасибо, ваш друг теперь может задать дополнительный вопрос!"}
+
+
+async def notify_user(callback_query: CallbackQuery, state: FSMContext):
+    message_text = await callback_query.message.answer(
+        f"Ваш друг перешел по ссылке и теперь может задать дополнительный вопрос!",
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Задать вопрос", callback_data="ask_free_question")]
+        ]),
+    )
+
+    await state.update_data(message_text_id=message_text.message_id)
+    await save_message_id(state, message_text.message_id)
