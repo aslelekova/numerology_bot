e8897bb7b2ca0d9309d08bf36c8815b1ed5230a1
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-10-09 18:45:06 +0300

    update

---
+++ create_db.py
@@ -0,0 +1,20 @@
+import aiosqlite
+import asyncio
+
+async def create_db():
+    async with aiosqlite.connect('./users.db') as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS login_id (
+                id INTEGER PRIMARY KEY,
+                tariff TEXT DEFAULT 'none',
+                readings_left INTEGER DEFAULT 0,
+                questions_left INTEGER DEFAULT 1, 
+                subscription_active BOOLEAN DEFAULT 0,
+                referred_id INTEGER DEFAULT NULL
+            )
+        """)
+        await db.commit()
+        print("Database created successfully.")
+
+if __name__ == "__main__":
+    asyncio.run(create_db())
